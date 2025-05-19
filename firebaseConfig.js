c12b4490daf90edd808cca4eb829b72cd9205f43
PLAAKKON <pasi.laakkonen@hotmail.com>
2025-05-19 11:12:44 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "urapolku-7780a.firebaseapp.com",
-    projectId: "urapolku-7780a",
-    storageBucket: "urapolku-7780a.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAnfsX522_ybXR7yhnAJbxDjuszVu4rZLE",
+  authDomain: "urapolku-7780a.firebaseapp.com",
+  projectId: "urapolku-7780a",
+  storageBucket: "urapolku-7780a.appspot.com",
+  messagingSenderId: "314066598772",
+  appId: "1:314066598772:web:50183db006293290378a21",
+  measurementId: "G-83QWJEC5SS"
 };
 
-// Initialize Firebase only if not already initialized
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
